dec0da4f8e6b5e8e88439a65415988eaedec1e60
Colten Glover <70673003+colllten@users.noreply.github.com>
2021-04-16 00:15:52 -0400

    Added Buttons up top / Changed Layout Manager
    No home button is needed since you are already at the home page, Layout manager is now GridBagLayout, Logout function has been added too

--- MainPage.java
+++ MainPage.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainPage extends JFrame {
 
@@ -7,23 +9,54 @@ public class MainPage extends JFrame {
     JPanel panel;
 
     public MainPage(User user) {
+        //FRAME SETUP//
         setTitle("Welcome " + user.getFirstName());
         setSize(new Dimension(600, 600));
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLayout(new BorderLayout());
-        profile = new JButton("Profile");
         panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        panel.add(profile, BorderLayout.EAST);
-        this.add(panel, BorderLayout.CENTER);
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints g = new GridBagConstraints();
+        //FRAME SETUP//
+
+        //BUTTONS//
+        g.anchor = GridBagConstraints.CENTER;
+        g.weightx = 0.5;
+
+        g.gridx = 0;
+        g.gridy = 0;
+        profile = new JButton("My Profile");
+        panel.add(profile, g);
+
+        g.gridx++;
+        JButton explore = new JButton("Explore");
+        panel.add(explore, g);
+
+        g.gridx++;
+        JButton notifications = new JButton("Notifications");
+        panel.add(notifications, g);
+
+        g.gridx++;
+        JButton friendsList = new JButton("Friends");
+        panel.add(friendsList, g);
+
+        g.gridx++;
+        JButton logout = new JButton("Logout");
+        logout.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                user.setOnline(false);
+                setVisible(false);
+                dispose();
+            }
+        });
+        panel.add(logout, g);
+
+        //BUTTONS//
 
         //SEARCH FUNCTION//
-        JLabel search = new JLabel("Search: ");
-        JTextField searchTxt = new JTextField(15);
-        panel.add(search, BorderLayout.NORTH);
-        panel.add(searchTxt, BorderLayout.NORTH);
         //SEARCH FUNCTION//
 
+        this.add(panel, BorderLayout.NORTH);
         setLocationRelativeTo(null);
         setVisible(true);
     }
