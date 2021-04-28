6611f56a74f6029ac10231dbb22ec007ab94548f
jeff-woodhouse <79885272+jeff-woodhouse@users.noreply.github.com>
2021-04-27 21:54:56 -0400

    Add functionality for new visibility parameter

--- Explore.java
+++ Explore.java
@@ -6,21 +6,8 @@ import java.util.*;
 import javax.swing.*;
 
 public class Explore extends JFrame {
-	
 
-	public static void main(String[] args) {
-		ArrayList<User> users = new ArrayList<User>();
-		for (int i = 0; i < 100; i++) {
-			users.add(new User("user" + i, "password", 1999, "User" + i, "Last" + i, null, null, null, false, null, "bio", "interests", null));
-		}
-		User u = new User("user32", "password", 1999, "User32", "Last32", null, null, null, true, null, "bio", "interests", null);
-		SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-            	Explore e = new Explore(u, users);
-            }
-        });
-	}
-	
+	//create new explore object
 	public Explore(User u, ArrayList<User> users) {
 		JFrame explore = new JFrame("Explore");
         explore.setLocationRelativeTo(null);
@@ -30,56 +17,79 @@ public class Explore extends JFrame {
 		String[] people = new String[users.size()];
 		ArrayList<User> otherUsers = new ArrayList<User>();
 		otherUsers.addAll(users);
+		//remove current user and any private users from list of all visible users
 		for (int i = 0; i < users.size(); i++) {
 			String username = u.getUsername();
 			User user = users.get(i);
 			if (username.equals(user.getUsername())){
 				otherUsers.remove(i);
 			}
+			if (user.getVisibility().equals("private")){
+				otherUsers.remove(i);
+			}
 		}
 
 		ArrayList<JPanel> panels = new ArrayList<JPanel>();
-
+		//create a JPanel for each user
 		if (otherUsers != null) {
 			for (int i = 0; i < otherUsers.size(); i++) {
 				JPanel panel = new JPanel();
+				panel.setLayout(new FlowLayout(FlowLayout.LEFT));
 				JButton profileButton = new JButton("View Profile");
 				int finalI = i;
+				User user = otherUsers.get(i);
+
+				//create button to view user profile
 				profileButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						System.out.println(otherUsers.get(finalI).getUsername() + "'s Profile");
+						System.out.println(user.getUsername() + "'s Profile");
 					}
 				});
+
+				//create a button to send a friend request
 				JButton friendReqButton = new JButton("Send Friend Request");
 				friendReqButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						// NEW FRIEND REQUEST METHOD IN USE //
-						System.out.println(otherUsers.get(finalI).getUsername() + " friend request");
-						u.sendFriendReq(u, otherUsers.get(finalI));
+						System.out.println(user.getUsername() + " friend request");
+						u.sendFriendReq(u, user);
 						System.out.println("Main User sent request" + u.getSentReqs().get(0));
-						System.out.println("Receiver received: " + otherUsers.get(finalI).getReceivedReqs().get(0));
+						System.out.println("Receiver received: " + user.getReceivedReqs().get(0));
 						// NEW FRIEND REQUEST METHOD IN USE //
 					}
 				});
-				JLabel name = new JLabel(otherUsers.get(i).getFirstName() + " " + otherUsers.get(i).getLastName());
-				if (otherUsers.get(i).getFriends() != null) {
-					if (otherUsers.get(i).getFriends().contains(u)) {
-						panel.add(name);
+
+				//create a new JLabel to display the user's name
+				JLabel name = new JLabel(user.getFirstName() + " " + user.getLastName());
+				panel.add(name);
+
+				//if the user is public, add buttons to see their profile and send a friend request...
+				//...if the users aren't already friends
+				if (user.getVisibility().equals("public")) {
+					if (user.getFriends().contains(u)) {
 						panel.add(profileButton);
 					} else {
-						panel.add(name);
 						panel.add(profileButton);
 						panel.add(friendReqButton);
 					}
-				} else {
-					panel.add(name);
-					panel.add(profileButton);
-					panel.add(friendReqButton);
+				//if the user is private, add a button to send a friend request if users are not friends
+				//and if they are friends add a button to view profile
+				} else if (user.getVisibility().equals("private")){
+					if (user.getFriends().contains(u)) {
+						panel.add(profileButton);
+					} else {
+						panel.add(friendReqButton);
+					}
+				}
+				JLabel online = new JLabel("Online");
+				if (user.isOnline()){
+					panel.add(online);
 				}
 				panels.add(panel);
 			}
 		}
 
+		//create a JScrollPane to contain all of the Jpanels created for each user
 		JPanel panelScroll = new JPanel();
 		panelScroll.setLayout(new GridLayout(panels.size(), 1));
 		
@@ -89,7 +99,8 @@ public class Explore extends JFrame {
 
 		JScrollPane scrollPane = new JScrollPane(panelScroll);
 		scrollPane.getVerticalScrollBar().setUnitIncrement(16);
-		
+
+		//add other panels and buttons to make it look pretty and be able to go home
 		JPanel topPanel = new JPanel();
 		JButton home = new JButton("Home");
 		home.addActionListener(new ActionListener () {
@@ -110,6 +121,4 @@ public class Explore extends JFrame {
 		explore.setVisible(true);
 		
 	}
-	
-
 }
