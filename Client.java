4a00d05dd298781f2d00b7b5a52fee1cbc03a963
Sudhanva02 <82622356+Sudhanva02@users.noreply.github.com>
2021-05-02 22:09:54 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,6 +2,14 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/**
+ * Client class that allows us to run program
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2021 -- Project 5
+ * @author Sudhanva Bharadwaj, Colten Glover, Brayden Hall, Japneet Mavi, Jeff Woodhouse
+ * @version May 3, 2021
+ */
+
 public class Client {
 
     private static String ip = "localhost";
