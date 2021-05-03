bd1499c37afd6d58b8fd137d281e5a897e2016f6
Sudhanva02 <82622356+Sudhanva02@users.noreply.github.com>
2021-05-02 23:22:33 -0400

    Update AllProfiles.java
    Commented code

--- AllProfiles.java
+++ AllProfiles.java
@@ -23,14 +23,15 @@ public class AllProfiles extends JFrame {
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
                 currentUser.setOnline(false);
-                Server.writeToFile();
+                Server.writeToFile(); // Used to save data even when Server is shut down. Data written to and from a txt file
             }
         });
-        setTitle(user.getUsername() + "'s Profile");
+        setTitle(user.getUsername() + "'s Profile"); // Layout with username of current user displayed at the top
         setLayout(new BorderLayout());
         setSize(500, 500);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        Border br = BorderFactory.createLineBorder(Color.YELLOW, 5);
+        Border br = BorderFactory.createLineBorder(Color.YELLOW, 5); 
+       
 
         JPanel jp = new JPanel();
         jp.setBackground(Color.PINK);
@@ -39,16 +40,16 @@ public class AllProfiles extends JFrame {
 
         JLabel pic;
         pic = currentUser.getIcon();
-        JButton toReturn = new JButton("Return to Main Page");
+        JButton toReturn = new JButton("Return to Main Page"); // Button that allows users to return to the home page 
         toReturn.setOpaque(true);
         toReturn.setBackground(Color.CYAN);
         toReturn.setBorder(br);
         toReturn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
-                    setVisible(false);
+                    setVisible(false); 
                     dispose();
-                    new MainPage(currentUser);
+                    new MainPage(currentUser); // New Main Page is created and the previous page is set to not visible
                 } catch (IOException ioex) {
                     //
                 }
@@ -56,19 +57,19 @@ public class AllProfiles extends JFrame {
         });
 
 
-        JLabel username = new JLabel(user.getUsername());
+        JLabel username = new JLabel(user.getUsername()); 
         username.setOpaque(true);
         username.setBackground(Color.CYAN);
         username.setBorder(br);
 
-        JLabel aboutMe = new JLabel("BIO : " + user.getBio());
+        JLabel aboutMe = new JLabel("BIO : " + user.getBio()); // Creates a bio with the bio the user entered at registartion
 
-        JButton friends = new JButton("Friends");
+        JButton friends = new JButton("Friends"); // A button a user can select to view all friends
         friends.setOpaque(true);
         friends.setBackground(Color.CYAN);
         friends.setBorder(br);
         friends.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e) { // If the button is clicked, the previous page they were on is not visible, as friends is shown
                 setVisible(false);
                 dispose();
                 new FriendsProfile(user);
@@ -95,11 +96,11 @@ public class AllProfiles extends JFrame {
          }
          }); */
 
-        JButton contact = new JButton("Contact Info");
+        JButton contact = new JButton("Contact Info"); // Contact info button is created
         contact.setOpaque(true);
         contact.setBackground(Color.CYAN);
         contact.setBorder(br);
-        contact.addActionListener(new ActionListener() {
+        contact.addActionListener(new ActionListener() { // If the contact info button is clicked, user's email is shown
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null, user.getEmail(), "Email", JOptionPane.INFORMATION_MESSAGE);
             }
@@ -110,23 +111,23 @@ public class AllProfiles extends JFrame {
 
         gbc.gridx = 1;
         gbc.gridy = 1;
-        jp.add(pic, gbc);
+        jp.add(pic, gbc); // Used for user's profile picture
 
         username.setPreferredSize(new Dimension(150, 50));
         gbc.gridx = 1;
         gbc.gridy = 0;
-        jp.add(username, gbc);
+        jp.add(username, gbc); // username display
 
         aboutMe.setPreferredSize(new Dimension(150, 50));
         gbc.gridx = 1;
         gbc.gridy = 2;
-        jp.add(aboutMe, gbc);
+        jp.add(aboutMe, gbc); // bio display
 
         friends.setPreferredSize(new Dimension(150, 30));
         gbc.gridx = 0;
         gbc.gridy = 3;
         gbc.ipadx = 1;
-        jp.add(friends, gbc);
+        jp.add(friends, gbc); // friends display
 
         String interest = String.valueOf(user.getInterests() + "\n");
         String[] rest = interest.split(",");
@@ -136,7 +137,7 @@ public class AllProfiles extends JFrame {
         int y = 2;
         Border b = BorderFactory.createLineBorder(Color.PINK, 3);
 
-        for(int i = 0; i < (user.getInterests()).size(); i++) {
+        for(int i = 0; i < (user.getInterests()).size(); i++) { // Loops through all the user's interests
             interests = new JLabel((i + 1) + " " +((user.getInterests()).get(i)));
             interests.setBorder(b);
             interests.setOpaque(true);
@@ -145,7 +146,7 @@ public class AllProfiles extends JFrame {
             y = y + 1;
             gbc.gridy = y;
 
-            jp.add(interests, gbc);
+            jp.add(interests, gbc); // List of user's interests is displayed on Profile
 
 
 
@@ -163,7 +164,7 @@ public class AllProfiles extends JFrame {
         contact.setPreferredSize(new Dimension(150, 30));
         gbc.gridx = 2;
         gbc.gridy = 3;
-        jp.add(contact, gbc);
+        jp.add(contact, gbc); // contact button aded
 
         toReturn.setPreferredSize(new Dimension(150, 30));
         gbc.gridx = 0;
