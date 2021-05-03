33f15fe38adfbd94037440c11fe5920a0793f560
Sudhanva02 <82622356+Sudhanva02@users.noreply.github.com>
2021-05-02 22:09:18 -0400

    Update AllProfiles.java
    Added Javadoc

--- AllProfiles.java
+++ AllProfiles.java
@@ -8,6 +8,14 @@ import java.io.IOException;
 import javax.swing.BorderFactory;
 import javax.swing.border.Border;
 
+/**
+ * Class for for all profiles on avalible to view
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2021 -- Project 5
+ * @author Sudhanva Bharadwaj, Colten Glover, Brayden Hall, Japneet Mavi, Jeff Woodhouse
+ * @version May 3, 2021
+ */
+
 public class AllProfiles extends JFrame {
 
     public AllProfiles(User user, User currentUser) {
